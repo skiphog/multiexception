8f20f18962ccaac95bab0e69c7f038ad0b984a3f
Skip <skiphog@mail.ru>
2017-06-28 13:26:09 +0300

    add comments

--- src/MultiException.php
+++ src/MultiException.php
@@ -2,10 +2,23 @@
 
 namespace Skiphog;
 
+/**
+ * Class MultiException
+ * @package Skiphog
+ */
 class MultiException extends \Exception implements \IteratorAggregate, \Countable
 {
+    /**
+     * Коллекция исключений
+     * @var array $data
+     */
     protected $data = [];
 
+    /**
+     * Добавить искючение в коллекцию
+     * @param \Throwable $e
+     * @return $this
+     */
     public function add(\Throwable $e)
     {
         $this->data[] = $e;
@@ -13,11 +26,18 @@ class MultiException extends \Exception implements \IteratorAggregate, \Countabl
         return $this;
     }
 
+    /**
+     * @return bool
+     */
     public function isEmpty(): bool
     {
         return empty($this->data);
     }
 
+    /**
+     * Получить все сообщения в виде массива
+     * @return array
+     */
     public function toArray(): array
     {
         return array_map(function ($value) {
@@ -26,11 +46,19 @@ class MultiException extends \Exception implements \IteratorAggregate, \Countabl
         }, $this->data);
     }
 
+    /**
+     * Реализация Iterator
+     * @return \ArrayIterator
+     */
     public function getIterator(): \ArrayIterator
     {
         return new \ArrayIterator($this->data);
     }
 
+    /**
+     * Реализация интерфейса Countable
+     * @return int
+     */
     public function count(): int
     {
         return count($this->data);
