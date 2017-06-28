f2f0245ebb06abcca92ff39c2095960ff7604fe7
Skip <skiphog@mail.ru>
2017-06-28 13:02:10 +0300

    change test

--- tests/test.php
+++ tests/test.php
@@ -5,9 +5,9 @@ require __DIR__ . '/../src/MultiException.php';
 
 $multi = new MultiException();
 
-$multi->add(new Exception('Жопа'));
-$multi->add(new Exception('Писька'));
-$multi->add(new Exception('Пиписька'));
-$multi->add(new Exception('Сиська'));
+$multi->add(new Exception('Error #1'));
+$multi->add(new Exception('Error #2'));
+$multi->add(new Exception('Error #3'));
+$multi->add(new Exception('Error #4'));
 
 //var_dump($multi instanceof ArrayAccess);
